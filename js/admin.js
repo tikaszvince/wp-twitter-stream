e9458d6cd6a1e4ad082d77beb07d2033711c1561
tikaszvince <vince.tikasz@gmail.com>
2013-08-04 15:15:30 +0200

    remove debug

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,6 @@
 (function ($) {
   "use strict";
   $(function () {
-    console.log('sss');
     $('body').on('change', '.wp-twitter-stream-filter-mode', function(){
       var hashtags = $(this).parents('form:first').find('.wp-twitter-stream-hashtag-list');
       if ($(this).val() == 0) {
