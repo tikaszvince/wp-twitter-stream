114bd89fa7f4b8722e51d7442c704ef945ca4093
tikaszvince <vince.tikasz@gmail.com>
2013-08-04 13:33:18 +0200

    default widget template

--- include/views/widget.php
+++ include/views/widget.php
@@ -9,6 +9,42 @@
  * @copyright 2013 Vince Tikász
  */
 
-?>
+/**
+ * Widget instance variables
+ *
+ * @var String $name Sidebar name.
+ * @var string $id Sidebar id
+ * @var String $description Sidebar description
+ * @var string $class Sidebar class
+ * @var string $before_widget String to display before widget
+ * @var string $after_widget String to display after widget
+ * @var string $before_title String to display before widget title
+ * @var string $after_title String to display after widget title
+ * @var string $widget_id The HTML id of the widget
+ * @var string widget_name The registered name of the widget
+ */
 
-<!-- This file is used to markup the public-facing widget. -->
+/**
+ * Widget variables
+ * @var WP_Twitter_Stream_Widget $widget Current widget instance
+ * @var array $tweets List of WP_Twitter_Stream_Tweet objects.
+ * @var string $display_title
+ * @var array $templates list of candidate template files.
+ * @var string $template_file The used template path.
+ */
+
+var_dump($templates, $template_file);
+echo $before_widget;
+  echo $display_title;
+
+  echo '<ol class="twitter-stream">';
+  foreach ($tweets as $tweet) {
+    /** @var WP_Twitter_Stream_Tweet $tweet */
+    echo
+      '<li>',
+        $tweet->display(),
+      '</li>';
+  }
+  echo '</ol>';
+
+echo $after_widget;
